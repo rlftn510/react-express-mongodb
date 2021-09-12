4b18045a0c2d3927b139d519b32a3bc32250f047
류길수 <rlftn510@naver.com>
2021-09-13 01:49:16 +0900

    로그아웃 기능

--- server/index.js
+++ server/index.js
@@ -76,13 +76,22 @@ app.get("/api/users/auth", auth, (req, res) => {
   //여기 까지 미들웨어를 통과해 왔다는 얘기는 auth 통과했다는 말
   res.status(200).json({
     _id: req.user._id,
-    isAdmin: req.user.rele === 0 ? false : true,
+    isAdmin: req.user.role === 0 ? false : true,
     isAuth: true,
     email: req.user.email,
     name: req.user.name,
     lastname: req.user.lastname,
     role: req.user.role,
     image: req.user.image,
+  });
+});
+
+app.get(`/api/users/logout`, auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.status(200).send({
+      success: true,
+    });
   });
 });
 
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,8 +1,8 @@
 const { User } = require("../models/User");
-const auth = (req, res, next) => {
+let auth = (req, res, next) => {
   // 인증 처리
   // 클라이언트에서 쿠키를 가져온다
-  let token = req.cookie.x_auth;
+  let token = req.cookies.x_auth;
   // 토큰을 복호화 한 후 유저를 찾는다.
   User.findByToken(token, (err, user) => {
     if (err) throw err;
